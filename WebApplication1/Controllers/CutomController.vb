f4caaf52dafca7988e0be58b07a8d75eec54d69b
Ace <getsyst@gmail.com>
2015-08-17 10:59:30 +0700

    s

---
+++ WebApplication1/Controllers/CutomController.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class CutomController
+        Inherits Controller
+
+        ' GET: Cutom
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Cutom/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Cutom/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Cutom/Create
+        <HttpPost()>
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Cutom/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Cutom/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Cutom/Delete/5
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Cutom/Delete/5
+        <HttpPost()>
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
